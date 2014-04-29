29c5f43c9d4acb712fba1431272e8ab8ad536235
flannimal <flanaganm09@gmail.com>
2014-04-28 18:37:39 -0700

    phase 2

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ActiveRecord::Base
-	has_many :ingredients
+	has_many :quantities
+	has_many :ingredients, through: :quantities
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 
-
-# TODO: Create your own seed data!
+ingredients = Ingredient.create([
+	{name: "succulent chicken feet", brand: "huang's", image: ""},
+	{name: "hot sauce", brand: "sweet baby ray's", image: ""},
+	{name: "broth", brand: "aldi", image: ""}
+	])
